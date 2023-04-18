39438b0849e1ce23fc9eb23bcbf4526912d3da8c
Domenic Ferri <domferri135@gmail.com>
2023-04-17 21:42:35 -0400

    Implemented ability to read temperature and transmit.

--- AirSensor.ino
+++ AirSensor.ino
@@ -1,6 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include "DHT.h"
 #include "config.h"
+
+//Constants
+#define DHTPIN 4 // The pin that the sensor connects to
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
 
 WiFiUDP udp;
 IPAddress broadcast;
@@ -8,22 +14,62 @@ uint16_t port;
 
 void setup() {
   Serial.begin(115200);
+  dht.begin();
   setupWifi();
 }
 
 void loop() {
-  sendHelloMessage();
+  //sendHelloMessage();
+  //delay(500);
+
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  sendHelloMessage(f);
+
   delay(500);
 }
 
-void sendHelloMessage() {
+void sendHelloMessage(float f) {
   Serial.println("Sending hello message...");
-  String message = "Hello!";
+  String message = "Hello";
   char rawMessage[6];
 
   message.toCharArray(rawMessage, 6);
+
   udp.beginPacketMulticast(broadcast, port, WiFi.localIP());
   udp.write(rawMessage);
+  udp.write(reinterpret_cast <uint8_t *>(&f), sizeof(f));
   udp.endPacket();
   yield();
 }
